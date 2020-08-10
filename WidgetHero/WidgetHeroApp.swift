7d9c2e6cd2af528826b46c5cfbbc1a4e0d4f5902
Atil Samancioglu <samancioglu.atil@gmail.com>
2020-08-10 19:09:30 +0300

    Initial Commit

---
+++ WidgetHero/WidgetHeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetHeroApp.swift
+//  WidgetHero
+//
+//  Created by Atil Samancioglu on 10.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct WidgetHeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
